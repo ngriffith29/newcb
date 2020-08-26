2c310631170022263520af7278426f55ced479b4
unknown <ngriffith29@student.egcc.edu>
2020-08-26 11:49:20 -0400

    front-end working

---
+++ src/components/Broken.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react'
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import axios from 'axios'
+
+
+function Broken(props) {
+    // let ClickH = () =>{ 
+    //     console.log('hi')
+    //     props.history.goBack()
+    // }
+
+    const [grade, setGrade] = useState(["Pick A Grade",6, 7, 8])
+    const [pickedGrade, setPickedGrade] = useState(grade[0])
+    const [issues, setIssues] = useState(["Pick an Issue","Broken Screen", "Broken Keys", 'fried motherboard'])
+    const [pickedIssue, setPickedIssue] = useState(issues[0])
+    const[input, setInputer] = useState("")
+
+let handleInputChange = (event) => {
+    setInputer(event.target.value)
+    
+}
+
+    let handleChange = (event) => {
+        setPickedGrade(event.target.value)
+
+    }
+let handleIssues = (event) => {
+    setPickedIssue(event.target.value)
+}
+    let handleSubmit = (event) => {
+        event.preventDefault()
+        axios.post('http://localhost:3000/students/', {
+            assetTag: input,
+            brokeReason: pickedIssue,
+            grade: pickedGrade
+
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+
+          setPickedGrade(grade[0])
+          setInputer("")
+          setPickedIssue(issues[0])
+          props.history.goBack()
+    }
+
+
+    return (
+        <div className="form-container">
+            <Form onSubmit={handleSubmit}>
+            <FormGroup>
+            <Label htmlFor="asset">Scan Your Asset Id</Label>
+            <Input id="asset" onChange={handleInputChange} value={input}   />
+
+            </FormGroup>
+
+
+
+
+                <FormGroup>
+                <Label for="grade">Pick Your Grade</Label>
+
+                <Input type="select" id="grade" value={pickedGrade} onChange={handleChange} >
+                    {
+                        grade.map((grade) => {
+                            return <option value={grade}>{grade}</option>
+                        })
+                    }
+                </Input>
+
+                </FormGroup>
+
+
+
+
+
+
+
+                    <FormGroup>
+                <Label htmlFor="issues">Pick your Problem</Label>
+
+                <Input type="select" id="issues" value={pickedIssue} onChange={handleIssues} >
+                    {
+                     issues.map((grade) => {
+                            return <option value={grade}>{grade}</option>
+                        })
+                    }
+                </Input>
+                </FormGroup>
+
+                <Button type="submit" color="primary">Submit and get grab new Chromebook</Button>
+
+            </Form>
+        </div>
+    )
+}
+
+export default Broken
---
+++ src/components/Home.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Jumbotron, Button } from 'reactstrap';
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+
+import './home.css'
+
+
+
+
+function Home() {
+
+    
+  
+
+
+    return (
+    <div className="body">
+   <div>
+      <Jumbotron className="jumboTron">
+        <h1 className="display-5">Welcome to the Chromebook Checkout</h1>
+        <p className="lead">Click the button below to exchange your chromebook</p>
+        <hr className="my-2" />
+       
+        <p className="lead">
+        <Link to='/broken'>         <Button color="primary">Learn More</Button> </Link>
+ 
+        </p>
+      </Jumbotron>
+    </div>
+
+
+
+    </div>
+     
+
+    
+    )
+}
+
+export default Home
